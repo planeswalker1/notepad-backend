9a19b729f2892ff4e5423953250971e192f3d99e
Daniel Munoz <daniel.munozdev@gmail.com>
2019-06-15 13:46:19 -0700

    remove console logs, update comments and if statements as per #1

--- controllers/auths.js
+++ controllers/auths.js
@@ -2,109 +2,136 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/schemas/user');
 const config = require('../models/config');
 
-// this function is going to create a token and assign it to a user
-// this works by checking:
-  // if the request has a email and password
-  // check the user db for a user with that email
-  // check if the passwords match
-  // if all are true
-    // create and return a token containing the users: id, and notes
 
+/**
+  login a user
+  @param {String} req.body.email - email sent by user
+  @param {String} req.body.password - password sent by user
+  @return {Object} - token in an object
+*/
 exports.loginUser = function (req, res, next) {
-  console.log('loginUser in api ran')
-  
-  if (typeof req.body.email !== 'string')
+  // console.log('loginUser in api ran')
+  // **validate inputs
+  if (typeof req.body.email !== 'string') {
     return res.status(400).send('Missing email');
-  if (typeof req.body.password !== 'string')
+  }
+  if (typeof req.body.password !== 'string') {
     return res.status(400).send('Missing password');
+  }
   
-  
-  console.log('looking for user');
+  // console.log('finding user');
+  // **find user
   User.findOne({ email: req.body.email }, function (err, user) {
-    if (err) return next(err);
-    if (!user)
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
       return res.status(400).send('No user with that email');
+    }
 
-    console.log('found user', user);
-    console.log('comparing password');
+    // console.log('found user', user);
+    // console.log('comparing password');
+    // **compare provided password with hash
     user.comparePassword(req.body.password, function (err, isMatch) {
-      if (err) return next(err);
-      if (!isMatch)
+      if (err) {
+        return next(err);
+      }
+      if (!isMatch) {
         return res.status(401).send('Incorrect password');
+      }
 
-      console.log('creating token');
+      // **create token
+      // console.log('creating token');
       var payload = {
         id: user._id,
         email: user.email
       };
-      console.log('payload', payload);
+      // console.log('payload', payload);
       var token = jwt.sign(payload, config.secret);
-      console.log('token in loginUser', token)
+      // console.log('token in loginUser', token)
+      // **save token
       user.token = token;
 
       user.save(function (err, user) {
-        if (err) return next(err);
-
-        console.log('sending token');
+        if (err) {
+          return next(err);
+        }
+        // **send token
+        // console.log('sending token');
         return res.json({ token: token });
       });
     });
   });
 }
 
-// this function is going to search for a token in the req.body, url, or headers
-// try to decode the token
-  // set req.user to decoded token
-// else
-  // return 403 error
-// send to next middleware transporting the token
+/**
+  Validate token
+  @param {String} req.headers[x-access-token] - user token
+  @return {function} - call next()
+*/
 exports.validateToken = function (req, res, next) {
-  console.log('validateToken in api ran');
-  if (req.headers['x-access-token'])
-    console.log('token passed from front-end server headers', req.headers['x-access-token']);
+  // console.log('validateToken in api ran');
+  // if (req.headers['x-access-token']) {
+  //   console.log('token passed from front-end server headers', req.headers['x-access-token']);
+  // }
 
   var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  console.log('token that was caught', token);
-  if (!token)
+  // console.log('token that was caught', token);
+  // **check for token
+  if (!token) {
     return res.status(403).send('This endpoint requires a token');
-
+  }
+  // **decode token
   try {
     var decoded = jwt.verify(token, config.secret);
   } catch (err) {
     return res.status(403).send('Failed to authenticate token');
   }
 
-  console.log('looking for user');
+  // console.log('finding user');
+  // **find user
   User.findById(decoded.id, function (err, user) {
-    if (err)
+    if (err) {
       return next(err);
-    if (!user)
+    }
+    if (!user) {
       return res.status(403).send('Invalid user');
-    if (token !== user.token)
+    }
+    if (token !== user.token) {
       return res.status(403).send('Expired token');
+    }
 
-    console.log('found user');
-    console.log('user.token', user.token);
+    // **attach decoded token and token to request
+    // console.log('found user');
+    // console.log('user.token', user.token);
     req.user = decoded;
-    console.log('req.user', req.user);
+    // console.log('req.user', req.user);
     req.token = user.token;
-    console.log('req.token', req.token);
+    // console.log('req.token', req.token);
     next();
   });
 }
 
+/**
+  logout user
+  @param {String} req.user.id - user decoded token id
+  @return {Object} - response object
+*/
 exports.logOutUser = function (req, res, next) {
-  console.log('logOutUser ran');
+  // console.log('logOutUser ran');
   User.findById(req.user.id, function (err, user) {
-    if (err) return next(err);
-    if (!user)
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
       return res.status(404).send('No user with that ID');
-    console.log('found user', user);
+    }
+    // console.log('found user', user);
     user.token = '';
-    console.log('user token removed', user);
+    // console.log('user token removed', user);
     user.save(function (err, user) {
       if (err) return next(err);
-      console.log('user updated! deleted their token');
+      // console.log('user updated! deleted their token');
       return res.status(200).send('ok');
     });
   });
--- controllers/users.js
+++ controllers/users.js
@@ -2,59 +2,83 @@ const User = require('../models/schemas/user');
 const bcrypt = require('bcrypt');
 const config = require('../models/config')
 
-exports.getUsers = function (req, res, next) {
-  User.find({}, function (err, users) {
-    if (err) return next(err);
+/**
+  find all users in db and return them
+  @return {Object} - all users in the db
+*/
+// exports.getUsers = function (req, res, next) {
+//   User.find({}, function (err, users) {
+//     if (err) return next(err);
 
-    return res.json(users);
-  });
-};
+//     return res.json(users);
+//   });
+// };
 
+/**
+  find a user and return it
+  @param {String} req.user.id - user token id
+  @return {Object} - found user
+*/
 exports.getUserById = function (req, res, next) {
-  console.log('req.user in api hit getUserById()', req.user);
+  // console.log('req.user in api hit getUserById()', req.user);
+  // **find a user and populate it's pages with page info
   User.findById(req.user.id)
     // this works idk why
     // old jank version
     // { 'pages', { _id: 1, name: 1, text: 1 }   
     .populate({ path: 'pages', select: '_id name text' })
     .exec(function (err, user) {
-      if (err) next(err);
-      if (!user)
+      if (err) {
+        next(err);
+      }
+      if (!user) {
         return res.status(404).send('No user with that ID');
+      }
 
-      console.log('found user', user);
+      // console.log('found user', user);
       return res.json(user);
     });
 };
 
+/**
+  create a user
+  @param {String} req.body - object containing user info
+  @param {String} req.body.email - user email
+  @param {String} req.body.password - user password
+  @return {Object} - response object
+*/
 exports.createUser = function (req, res, next) {
-  console.log('createUser called');
-  // validate inputs
-  if (typeof req.body.email !== 'string')
+  // console.log('createUser called');
+  // **validate inputs
+  if (typeof req.body.email !== 'string') {
     return res.status(400).send('Missing email');
-  if (typeof req.body.password !== 'string' && typeof req.body.hash !== 'string')
+  }
+  if (typeof req.body.password !== 'string' && typeof req.body.hash !== 'string') {
     return res.status(400).send('Missing password');
-  
+  }
   // http://emailregex.com
   if (!req.body.email || !(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(req.body.email))) {
     return res.status(400).send('Invalid email');
   }
 
-  // create userData from req
+  // **create userData from req.body to create new user
   var userData = {};
   userData.email = req.body.email;
-  if (req.body.password)
+  if (req.body.password) {
     userData.hash = req.body.password;
-  if (req.body.hash)
+  }
+  if (req.body.hash) {
     userData.hash = req.body.hash;
+  }
 
-  // create new user
-  console.log('userData for new user', userData)
+  // **create new user
+  // console.log('userData for new user', userData)
   var newUser = new User(userData);
   newUser.save(function (err, user) {
     if (err) {
-      if (err.code === 11000)
+      if (err.code === 11000) {
         return res.status(400).send('Email already registered');
+      }
       return next(err);
     }
 
@@ -62,48 +86,85 @@ exports.createUser = function (req, res, next) {
   });
 };
 
+/**
+  update a user
+  @param {String} req.user.id - user token id 
+  @param {Object} req.body - updated user info
+  @param {String} req.body.email - user updated email
+  @param {String} req.body.password - user updated password
+  @return {Object} - response object
+*/
 exports.updateUserById = function (req, res, next) {
   let userData = {};
 
-  if (req.body.email)
+  if (req.body.email) {
     userData.email = req.body.email;
+  }
 
-  if (req.body.password)
+  if (req.body.password) {
     userData.hash = req.body.password;
+  }
+  if (req.body.hash) {
+  userData.hash = req.body.hash;
+  }
 
-    if (req.body.hash)
-    userData.hash = req.body.hash;
-
-  // hash before saving
-  // since mongoose findByIdAndUpdate bypasses hooks
-  bcrypt.genSalt(config.saltRounds, function(err, salt) {
-    if (err) return console.log('error', err);
-
-    bcrypt.hash(userData.hash, salt, function(err, hash) {
+  // **hash before saving
+  // **since mongoose findByIdAndUpdate bypasses hooks
+  if (userData.hash) {
+    bcrypt.genSalt(config.saltRounds, function (err, salt) {
+      if (err) {
+        return console.log('error', err);
+      }
+      bcrypt.hash(userData.hash, salt, function (err, hash) {
         // Store hash in DB.
         userData.hash = hash;
 
-      User.findByIdAndUpdate(req.user.id, userData, { new: true, upsert: true }, function (err, user) {
+        User.findByIdAndUpdate(req.user.id, userData, { new: true, upsert: true }, function (err, user) {
           if (err) {
-            if (err.code === 11000)
+            if (err.code === 11000) {
               return res.status(400).send('Email already registered');
+            }
             return next(err);
           }
-
-          if (!user)
+          if (!user) {
             return res.status(404).send('No user with that ID');
+          }
+
           return res.sendStatus(200);
         });
+      });
     });
-  });
+  }
+  else {
+    User.findByIdAndUpdate(req.user.id, userData, { new: true, upsert: true }, function (err, user) {
+      if (err) {
+        if (err.code === 11000) {
+          return res.status(400).send('Email already registered');
+        }
+        return next(err);
+      }
+      if (!user) {
+        return res.status(404).send('No user with that ID');
+      }
+
+      return res.sendStatus(200);
+    });
+  }
 };
 
-exports.deleteUserById = function (req, res, next) {
-  User.findByIdAndDelete(req.params.id, function (err, user) {
-    if (err) return next(err);
-
-    if (!user)
-      return res.status(404).send('No user with that ID');
-    return res.sendStatus(200);
-  });
-};
+// /**
+//   delete a user in the db
+//   @param {String} req.params.id - user id in url
+//   @return {Object} - response object
+// */
+// exports.deleteUserById = function (req, res, next) {
+//   User.findByIdAndDelete(req.params.id, function (err, user) {
+//     if (err) {
+//       return next(err);
+//     }
+//     if (!user) {
+//       return res.status(404).send('No user with that ID');
+//     }
+//     return res.sendStatus(200);
+//   });
+// };
